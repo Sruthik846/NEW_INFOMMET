aeed40948b67a75ee833f30997cc64fff5927290
sruthi <sruthikallingal@gmail.com>
2023-08-10 10:05:28 +0530

    Loading issue and next tab issue solved

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,25 +5,15 @@ import { UserContext } from "../Context/UserContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import Cookies from "js-cookie";
+import Home from "../Home/Home";
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [showerrorMessage, setshowerrorMessage] = useState([]);
   const { updateUser } = useContext(UserContext);
-  const isAuth = localStorage.getItem('token') !== null;
+  const [authenticated, setAuthenticated] = useState(false);
   const navigate = useNavigate();
-  
-  // useEffect(() => {
-  //   if (!isAuth) {
-  //     // Redirect the user to the login page if not authenticated
-  //     navigate('/');
-  //   }
-  //   else{
-  //     navigate('/home');
-  //   }
-  // }, [isAuth, navigate]);
 
-  
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -66,18 +56,45 @@ function Login() {
           Cookies.set("user_type", response.data["user"]["user_type"]);
         });
       navigate("/home");
-      window.location.reload();
+      // window.location.reload();
     } catch (err) {
-      setshowerrorMessage(err.response["data"]['message']);
+      setshowerrorMessage(err.response["data"]["message"]);
       navigate("/");
     }
   };
 
+  useEffect(() => {
+    // Function to handle authentication change
+    const handleAuthChange = () => {
+      // Check if the token is present
+      const authToken = localStorage.getItem("token");
+      if (authToken) {
+        // Perform token validation logic if needed
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    };
+
+    // Listen for storage events (changes in localStorage)
+    window.addEventListener("storage", handleAuthChange);
+
+    // Initial check when the component mounts
+    handleAuthChange();
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("storage", handleAuthChange);
+    };
+  }, []);
 
   return (
-    <div className="relative flex flex-col justify-center overflow-hidden h-screen bg-white md:bg-gray-100 lg:bg-gray-100 ">
-
-{/* {showerrorMessage.length !== 0 && (
+    <>
+      {authenticated ? (
+        <Home></Home>
+      ) : (
+        <div className="relative flex flex-col justify-center overflow-hidden h-screen bg-white md:bg-gray-100 lg:bg-gray-100 ">
+          {/* {showerrorMessage.length !== 0 && (
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-6 md:p-8 lg:p-12 xl:p-16"
             style={{ backdropFilter: "blur(5px)" }}
@@ -116,67 +133,68 @@ function Login() {
           </div>
         )} */}
 
-
-      <div className="w-full p-6 m-auto rounded-md lg:max-w-sm flex  flex-col justify-center lg:shadow-lg bg-white">
-        <img
-          src="https://infolks.info/images/logo/logo-rc.svg"
-          className="w-full"
-          alt="Logo"
-          style={{ height: "150px" }}
-        ></img>
-        <h1 className="text-3xl font-bold text-left text-gray-600 py-10">
-          Hey! <br></br>
-          Login
-        </h1>
-        <form onSubmit={(e) => onSubmit(e)} method="POST">
-          <div className="flex items-center bg-gray-200 rounded-lg focus:outline-none focus:ring focus:ring-opacity-40 mb-4 px-2 ">
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => onChange(e)}
-              className="flex-grow block w-full text-gray-800 px-4 py-2 mt-2 text-sm border bg-gray-200 rounded-lg focus:outline-none focus:ring-opacity-40 focus:bg-gray-200 appearance-none bg-transparent "
-              placeholder="Infolks Email"
-              required
-            />
-            <div className="ml-2 text-gray-500">
-              <FontAwesomeIcon icon={faPencil} />
-            </div>
-          </div>
-
-          <div className="mb-2 flex items-center bg-gray-200 rounded-lg focus:outline-none focus:ring focus:ring-opacity-40 px-2">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              value={password}
-              onChange={(e) => onChange(e)}
-              className="flex-grow block w-full text-gray-800 px-4 py-2 mt-2 text-sm border bg-gray-200 rounded-lg focus:outline-none focus:ring-opacity-40"
-              placeholder="Password"
-              required
-            />
-            <div className="ml-2 text-gray-500">
-              <FontAwesomeIcon
-                onClick={() => setShowPassword(!showPassword)}
-                icon={showPassword ? faEyeSlash : faEye}
-              />
-            </div>
-          </div>
-          {showerrorMessage.length !== 0 && (
-            <div className="text-red-500 text-sm font-medium">
-              {showerrorMessage}
-            </div>
-          )}
-          <div className="mt-6 pb-5">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 text-sm tracking-wide bg-blue-800 text-white transition-colors duration-200 transform rounded-md hover:bg-blue focus:outline-none focus:bg-blue "
-            >
+          <div className="w-full p-6 m-auto rounded-md lg:max-w-sm flex  flex-col justify-center lg:shadow-lg bg-white">
+            <img
+              src="https://infolks.info/images/logo/logo-rc.svg"
+              className="w-full"
+              alt="Logo"
+              style={{ height: "150px" }}
+            ></img>
+            <h1 className="text-3xl font-bold text-left text-gray-600 py-10">
+              Hey! <br></br>
               Login
-            </button>
+            </h1>
+            <form onSubmit={(e) => onSubmit(e)} method="POST">
+              <div className="flex items-center bg-gray-200 rounded-lg focus:outline-none focus:ring focus:ring-opacity-40 mb-4 px-2 ">
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                  className="flex-grow block w-full text-gray-800 px-4 py-2 mt-2 text-sm border bg-gray-200 rounded-lg focus:outline-none focus:ring-opacity-40 focus:bg-gray-200 appearance-none bg-transparent "
+                  placeholder="Infolks Email"
+                  required
+                />
+                <div className="ml-2 text-gray-500">
+                  <FontAwesomeIcon icon={faPencil} />
+                </div>
+              </div>
+
+              <div className="mb-2 flex items-center bg-gray-200 rounded-lg focus:outline-none focus:ring focus:ring-opacity-40 px-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                  className="flex-grow block w-full text-gray-800 px-4 py-2 mt-2 text-sm border bg-gray-200 rounded-lg focus:outline-none focus:ring-opacity-40"
+                  placeholder="Password"
+                  required
+                />
+                <div className="ml-2 text-gray-500">
+                  <FontAwesomeIcon
+                    onClick={() => setShowPassword(!showPassword)}
+                    icon={showPassword ? faEyeSlash : faEye}
+                  />
+                </div>
+              </div>
+              {showerrorMessage.length !== 0 && (
+                <div className="text-red-500 text-sm font-medium">
+                  {showerrorMessage}
+                </div>
+              )}
+              <div className="mt-6 pb-5">
+                <button
+                  type="submit"
+                  className="w-full px-4 py-2 text-sm tracking-wide bg-blue-800 text-white transition-colors duration-200 transform rounded-md hover:bg-blue focus:outline-none focus:bg-blue "
+                >
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
